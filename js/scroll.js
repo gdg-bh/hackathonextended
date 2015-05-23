2896909899ab1cc0671dda5e09e1fef47617cf85
Tiago Porto <tiago@tiagoporto.com>
2015-05-22 21:49:06 -0300

    Problemas JS
    Removido dependencias sem usar
    Resolvido menu sumir ao clicar responsivo

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-  $('.navbar-nav').find('a').click(function(event) {
-    event.preventDefault();
-    var elementClicked = $(this).attr('href');
-    var dataGo = $(elementClicked);
+	$('.navbar-nav').find('a').click(function(event) {
+		var elementClicked = $(this).attr('href');
+		var dataGo = $(elementClicked);
 
-    $('html, body').stop().animate({
-      scrollTop: dataGo.offset().top - 105
-    }, 500);
-  });
+
+		$('#navbar').removeClass('in');
+
+		$('body').stop().animate({
+			scrollTop: dataGo.offset().top - 105
+		}, 500);
+
+
+		console.log($(this).parent().parent());
+
+		$(this).parent().addClass('active');
+
+
+
+		event.preventDefault();
+	});
 });
